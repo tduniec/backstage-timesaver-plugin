b9d68dda302ff56e168492d30b6a7ae81495dde5
Javier Prieto <javier.accounts@fiml.co>
2024-03-18 12:13:22 -0400

    Linted, formatted and created additional logging.

--- plugins/time-saver-backend/migrations/init.js
+++ plugins/time-saver-backend/migrations/init.js
@@ -67,8 +67,10 @@ exports.up = async function up(knex) {
       );
   }
 
+  await knex.schema
+    .createTable('ts_template_time_savings', (table) => {
       table.comment(
-        'Table contains template time savings with relation to the templateTaskId',
+        'Table contains template time savings with relation to the templateTaskId'
       );
 
       if (isPostgreSQL) {
@@ -93,20 +95,49 @@ exports.up = async function up(knex) {
         .notNullable()
         .defaultTo(knex.fn.now())
         .comment('The creation time of the record');
-      table.string('template_task_id').comment('Template task ID');
+
+      table
+      .string('template_task_id')
+      .comment('Template task ID');
+
       table
         .string('template_name')
         .comment('Template name as template entity_reference');
-      table.string('team').comment('Team name of saved time');
+
+      table
+        .string('team')
+        .comment('Team name of saved time');
+
       table
         .float('time_saved', 2)
         .comment('time saved by the team within template task ID, in hours');
-      table.string('template_task_status').comment('template task status');
+
+      table
+      .string('template_task_status')
+      .comment('template task status');
+
       table
         .string('created_by')
-        .comment('entity refernce to the user that has executed the template');
-    });
-  }
+        .comment('entity reference to the user that has executed the template');
+    })
+    .then(
+      (s) => {
+        response = {
+          ...response,
+          ts_template_time_savings: s,
+        };
+        console.log('Created table ts_template_time_savings.');
+      },
+      (reason) => {
+        response = {
+          ...response,
+          ts_template_time_savings: `Not created: ${reason}`,
+        };
+        console.log('Failed to create table ts_template_time_savings.');
+      }
+    );
+
+  return response;
 };
 
 /**
@@ -115,3 +146,4 @@ exports.up = async function up(knex) {
 exports.down = async function down(knex) {
   return knex.schema.dropTable('ts_template_time_savings');
 };
+
