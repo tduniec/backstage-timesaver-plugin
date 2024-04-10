7ab5102486e227e2035cac3b0cd36cb3c6fd9fd8
tduniec <tomekduniec@gmail.com>
2024-04-10 14:08:45 +0200

    cleanup

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-if [ "$(npm view $(cut -d "=" -f 2 <<< $(npm run env | grep "npm_package_name")) version)" == "$(cut -d "=" -f 2 <<< $(npm run env | grep "npm_package_version"))" ]; then echo true; else echo false; fi
