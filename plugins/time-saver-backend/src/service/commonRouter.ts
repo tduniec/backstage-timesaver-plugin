5884bf5f2f79171a7977dae03cf4168db240e4ba
Javier Prieto <javier.accounts@fiml.co>
2024-07-13 14:07:57 -0400

    chore: lint and format commonRouter.ts
    Signed-off-by: Javier Prieto <javier.accounts@fiml.co>

--- plugins/time-saver-backend/src/service/commonRouter.ts
+++ plugins/time-saver-backend/src/service/commonRouter.ts
@@ -35,9 +35,7 @@ export function setupCommonRoutes(
   });
 
   router.get('/getStats/', async (request, response) => {
-    const templateId = request.query.templateTaskId;
-    const team = request.query.team;
-    const templateName = request.query.templateName;
+    const { templateId, team, templateName } = request.query;
     let result;
     if (templateId) {
       result = await apiHandler.getStatsByTemplateTaskId(String(templateId));
