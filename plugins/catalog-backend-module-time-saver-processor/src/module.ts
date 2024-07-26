899bf5bfa1bcfe122f20d6fb6844bf416c243bf7
Javier Prieto <javier.accounts@fiml.co>
2024-07-26 09:18:51 -0400

    fix: add TimeSaverProcessor instantitation in module and add logger to it
    Signed-off-by: Javier Prieto <javier.accounts@fiml.co>

--- plugins/catalog-backend-module-time-saver-processor/src/module.ts
+++ plugins/catalog-backend-module-time-saver-processor/src/module.ts
@@ -2,15 +2,21 @@ import {
   coreServices,
   createBackendModule,
 } from '@backstage/backend-plugin-api';
+import { catalogProcessingExtensionPoint } from '@backstage/plugin-catalog-node/alpha';
+import { TimeSaverProcessor } from './processor/TimeSaverProcessor';
 
 export const catalogModuleTimeSaverProcessor = createBackendModule({
   pluginId: 'catalog',
   moduleId: 'time-saver-processor',
   register(reg) {
     reg.registerInit({
-      deps: { logger: coreServices.logger },
-      async init({ logger }) {
-        logger.info('Hello World!');
+      deps: {
+        logger: coreServices.logger,
+        catalog: catalogProcessingExtensionPoint,
+      },
+      async init({ logger, catalog }) {
+        logger.info('TimeSaver Catalog Processor ready.');
+        catalog.addProcessor(new TimeSaverProcessor(logger));
       },
     });
   },
